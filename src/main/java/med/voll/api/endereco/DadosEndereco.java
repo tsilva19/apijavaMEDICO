57a75d34f0eebe150075222029bc62dd5a05d7ed
Thiago <thiagoalves060@gmail.com>
2023-10-01 12:39:22 -0300

    criando DTO

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,4 +1,4 @@
-package med.voll.api.medico;
+package med.voll.api.endereco;
 
 public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf,  String complemento, String numero) {
 }
