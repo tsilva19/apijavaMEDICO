1e7cf9a248367b5e237b2dc3845b7572e6b56716
Thiago <thiagoalves060@gmail.com>
2023-10-01 11:46:05 -0300

    Initial commit

---
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,4 @@
+package med.voll.api.medico;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+}
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,11 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public class Medico {
+    private Long id;
+    private String nome;
+    private String email;
+    private Especialidade especialidade;
+    private Endereco endereco;
+}
